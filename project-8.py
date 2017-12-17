9fac1e7c0b2ec7bb5e13986d37c61d49683ca4f6
NadyaSakh <Nadyasaxorova@mail.ru>
2017-12-17 20:37:44 +1000

    second commit

--- project-8.py
+++ project-8.py
@@ -51,17 +51,22 @@ with b_con:
 
 #Получаем данные и записываем их в таблицы
 try:
-    url = "https://api.github.com/users?access_token=0f995f8b11d34f21842e26c95cdccd692379b799&page=2&per_page=70"
+    url = "https://api.github.com/users?page=2&per_page=70"
     response1 = urllib.request.urlopen(url)
     obj = json.loads(response1.read().decode())
 except urllib.error.URLError:
     print("Ошибка подключения")
+    try:
+        url = "https://api.github.com/users?access_token=0f995f8b11d34f21842e26c95cdccd692379b799&page=2&per_page=70"
+        response1 = urllib.request.urlopen(url)
+        obj = json.loads(response1.read().decode())
+    except urllib.error.URLError:
+        print("Ошибка подключения")
 repos = []
 
 with b_con:
     with b_con.cursor() as b_cur:
         for item in obj:
-            print("login =  ", item['login'], ", url_repo = ", item['repos_url'])
             repos.append(item['repos_url'])
             #добавить логины в табл юзеров
             qur_insert = "INSERT INTO users (login) VALUES ('{}');".format(item['login'])
@@ -80,8 +85,6 @@ with b_con:
         repo_count = 0
         with b_con.cursor() as b_cur:
             for tmp_repo in tmp_repos:
-                print("id_r = ", id_r, " repo_name = ", tmp_repo['name'], " time_created = ", tmp_repo['created_at'],
-                        ' id_u = ', id_u, ' stars = ', tmp_repo['stargazers_count'])
                 time = tmp_repo['created_at'].split(" ")
                 # добавить поля в таблицу репо
                 qur_insert = "INSERT INTO repos (name, time, id_u, stars) VALUES ('{}','{}', '{}', '{}');".format(tmp_repo['name'],
@@ -91,7 +94,6 @@ with b_con:
                 b_cur.execute(qur_insert)
                 b_con.commit()
 
-                print("language = ", tmp_repo['language'], " id_r = ", id_r)
                 # добавить в таблицу  репо
                 qur_insert = "INSERT INTO tags (language, id_r) VALUES ('{}','{}');".format(tmp_repo['language'],id_r)
                 b_cur.execute(qur_insert)
@@ -100,7 +102,6 @@ with b_con:
                 repo_count += 1
                 id_r += 1
         # записать число репозиториев в таблицу юзеров
-        print("repo_count = ", repo_count)
         with b_con.cursor() as b_cur:
             qur_insert = "UPDATE users SET repos_number = {} WHERE id_u = {};".format(repo_count, id_u)
             b_cur.execute(qur_insert)
